230c9318687cfa3ac1d70f8e80338394d912c2e4
Malak0277 <21P0277@eng.asu.edu.eg>
2023-05-11 02:42:51 +0300

    added subpages to django

--- django/project/project/pages/urls.py
+++ django/project/project/pages/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('birthday/', views.bday, name='bday'),
+    path('wedding/', views.wedding, name='wedding'),
+    path('eid/', views.eid, name='eid'),
+    path('minis/', views.minis, name='minis'),
+    path('others/', views.others, name='others'),
+
 ]
